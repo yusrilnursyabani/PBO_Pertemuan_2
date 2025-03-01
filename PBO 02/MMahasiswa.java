de291c45c0d881b6d71d39d7633c920fff375aae
yusrilnursyabani <yusrilnursabani12@gmail.com>
2025-03-01 23:44:39 +0700

    Add files via upload

--- PBO 02/MMahasiswa.java
+++ PBO 02/MMahasiswa.java
@@ -9,9 +9,9 @@ public class MMahasiswa {
         MataKuliah PBO = new MataKuliah("PBO", "Pemrograman Berorientasi Objek", 3);
         MataKuliah MBD = new MataKuliah("MBD", "Manajemen Basis Data", 3);
         
-        Mahasiswa M1 = new Mahasiswa("234", "Citra", "Informatika");
-        Dosen D1 = new Dosen("123", "Andi", "Informatika");
-        Kendaraan K1 = new Kendaraan("H1234AB", "motor");
+        Mahasiswa M1 = new Mahasiswa("181", "Yusril", "Informatika");
+        Dosen D1 = new Dosen("123", "Pak Nurdin", "Informatika");
+        Kendaraan K1 = new Kendaraan("G2365VN", "motor");
         
         M1.setDosenWali(D1);
         M1.setKendaraan(K1);
